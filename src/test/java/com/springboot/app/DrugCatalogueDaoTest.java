a529c8050ce58a79e25b0c4f401ce50375933a31
denglt <ltdeng@hotmail.com>
2020-08-18 14:47:23 +0800

    test mybatis plus selectPage

--- src/test/java/com/springboot/app/DrugCatalogueDaoTest.java
+++ src/test/java/com/springboot/app/DrugCatalogueDaoTest.java
@@ -1,8 +1,10 @@
 package com.springboot.app;
 
 
+import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.springboot.model.DrugCatalogue;
 
 import com.springboot.orm.user.DrugCatalogueDao;
@@ -34,15 +36,16 @@ public class DrugCatalogueDaoTest {
 
     @Autowired
     private DrugCatalogueDao drugCatalogueDao;
+
     @Test
-    public void updateDrugPinYinAndWb(){
+    public void updateDrugPinYinAndWb() {
         QueryWrapper<DrugCatalogue> wrapper = new QueryWrapper<>();
-        wrapper.eq("dp_id",217);//.last("limit 1");
+        wrapper.eq("dp_id", 217);//.last("limit 1");
         List<DrugCatalogue> drugCatalogues = drugCatalogueDao.selectList(wrapper);
 
-        drugCatalogues.forEach(drug ->{
+        drugCatalogues.forEach(drug -> {
             String drugName = drug.getDrugChemName();
-            String pinyin =HzUtils.getPinyinCap(drugName, HzUtils.CaseType.UPPERCASE);
+            String pinyin = HzUtils.getPinyinCap(drugName, HzUtils.CaseType.UPPERCASE);
             String wb = HzUtils.getWbCap(drugName, HzUtils.CaseType.UPPERCASE);
             DrugCatalogue drugCatalogue = new DrugCatalogue();
             drugCatalogue.setDrugPinyin(pinyin);
@@ -54,17 +57,33 @@ public class DrugCatalogueDaoTest {
     }
 
     /**
-     *  UpdateWrapper 可以直接 set 想要更新的字段，支持null
+     * UpdateWrapper 可以直接 set 想要更新的字段，支持null
      */
     @Test
-    public void update(){
+    public void update() {
         UpdateWrapper<DrugCatalogue> wrapper = new UpdateWrapper<>();
-        wrapper.eq("dp_id",217).like("drug_code","denglt%");
+        wrapper.eq("dp_id", 217).like("drug_code", "denglt%");
         DrugCatalogue drugCatalogue = new DrugCatalogue();
         drugCatalogue.setDrugCode("dengltxxxx");
-        wrapper.set("drug_wb",null); // drug_wb = null
-       // wrapper.set("drug_code",null);
+        wrapper.set("drug_wb", null); // drug_wb = null
+        // wrapper.set("drug_code",null);
         int update = drugCatalogueDao.update(drugCatalogue, wrapper);
         System.out.println("update count " + update);
+
+    }
+
+    @Test
+    public void selectPage() {
+        QueryWrapper<DrugCatalogue> wrapper = new QueryWrapper<>();
+        wrapper.eq("dp_id", 217);//.last("limit 1");
+        Page<DrugCatalogue> page = new Page<>();
+        page.setCurrent(3);
+        page.setSize(15);
+        Page<DrugCatalogue> pageData = drugCatalogueDao.selectPage(page, wrapper);
+        List<DrugCatalogue> data = pageData.getRecords();
+
+        String pageInfo = "current:" + page.getCurrent() + "; size:" + page.getSize() + "; total:" + page.getTotal() + "; pages:" + page.getPages();
+        System.out.println(pageInfo);
+
     }
 }
