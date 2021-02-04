d2e92c34466527aff7e9c07caba9180cbde0b0f0
denglt <ltdeng@hotmail.com>
2021-02-04 09:36:46 +0800

    简化配置

--- src/main/java/com/springboot/app/DemoApplication.java
+++ src/main/java/com/springboot/app/DemoApplication.java
@@ -18,11 +18,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@ComponentScan(value = {"com.springboot","com.yuntai"})
 @RestController
-@EnableAutoConfiguration(/*exclude = {DataSourceAutoConfiguration.class}*/) // spring.factories 文件中配置了  Auto Configure
 @EnableCaching
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.springboot","com.yuntai"}
+     /* exclude = {DataSourceAutoConfiguration.class} */)
 public class DemoApplication  {  // extends SpringBootServletInitializer  for spring boot war(部署到容器时使用)
 
     private static ConfigurableApplicationContext applicationContext;
