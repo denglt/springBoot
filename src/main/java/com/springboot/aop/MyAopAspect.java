ece501840ad2cc14a621c39d37944f0c4b60b4a1
denglt <ltdeng@hotmail.com>
2022-07-13 12:15:16 +0800

    @Aspect @Around  example 2

--- src/main/java/com/springboot/aop/MyAopAspect.java
+++ src/main/java/com/springboot/aop/MyAopAspect.java
@@ -10,7 +10,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 /**
- * @Description:
+ * @Description:   EnableAspectJAutoProxy
  * @Package: com.springboot.aop
  * @Author: denglt
  * @Date: 2022/7/13 10:52
