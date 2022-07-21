c767a768ad88e015664d9568496cc5e4c6e41e28
denglt <ltdeng@hotmail.com>
2022-07-21 11:45:57 +0800

    dev:读写分离的datasource

--- src/main/java/com/springboot/config/DataSourceConfig.java
+++ src/main/java/com/springboot/config/DataSourceConfig.java
@@ -7,11 +7,15 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- *  see to DataSourceAutoConfiguration
+ * see to DataSourceAutoConfiguration
  *
  * @Description:
  * @Package: com.springboot.config
@@ -32,7 +36,7 @@ public class DataSourceConfig {
     @Bean(name = "firstDataSource")
     @Primary
     @ConfigurationProperties(prefix = "app.datasource.db1.hikari")
-    public DataSource firstDataSource( DataSourceProperties dataSourceProperties) {
+    public DataSource firstDataSource(DataSourceProperties dataSourceProperties) {
         DataSource dataSource = dataSourceProperties.initializeDataSourceBuilder().build();
         return dataSource;
     }
@@ -50,5 +54,38 @@ public class DataSourceConfig {
         return dataSourceProperties.initializeDataSourceBuilder().build();
     }
 
+    /**
+     *   PlatformTransactionManager 和 Mybatic 配置该 datasource 可以按照交易的读写自动切换数据库，达到读写分离（未测试，应该没啥问题）
+     * @param masterDataSource
+     * @param slaveDataSource
+     * @return
+     */
+    @Bean(name = "transactionRoutingDataSource")
+    public DataSource routingDataSource(
+            @Qualifier("firstDataSource") DataSource masterDataSource,
+            @Qualifier("secondDataSource") DataSource slaveDataSource
+    ) {
+        TransactionRoutingDataSource routingDataSource = new TransactionRoutingDataSource();
+        Map<Object, Object> dataSourceMap = new HashMap<>();
+        dataSourceMap.put(DataSourceType.READ_WRITE, masterDataSource);
+        dataSourceMap.put(DataSourceType.READ_ONLY, slaveDataSource);
+
+        routingDataSource.setTargetDataSources(dataSourceMap);
+        routingDataSource.setDefaultTargetDataSource(masterDataSource);
+        return routingDataSource;
+    }
+    private class TransactionRoutingDataSource extends AbstractRoutingDataSource {
+
+        @Override
+        protected Object determineCurrentLookupKey() {
+            return TransactionSynchronizationManager.isCurrentTransactionReadOnly() ?
+                    DataSourceType.READ_ONLY : DataSourceType.READ_WRITE;
+        }
+    }
+
+    private enum DataSourceType {
+        READ_ONLY,
+        READ_WRITE
+    }
 
 }
