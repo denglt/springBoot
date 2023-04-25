abe07ec8d06b97acfeb1359c186463b143b453bf
denglt <ltdeng@hotmail.com>
2023-04-25 14:04:22 +0800

    study @DubboReference in dubbo 2.7.15 2

--- src/main/java/com/springboot/config/HdpAccessConfig.java
+++ src/main/java/com/springboot/config/HdpAccessConfig.java
@@ -21,10 +21,13 @@ public class HdpAccessConfig {
          dubbo 2.7.15
          ReferenceAnnotationBeanPostProcessor 将会在spring中注册一个ReferenceBean<AccessHospitalHandler>
          其他Service 可以使用 @Resource @Autowired 直接获取
-         单这儿有个必要的前提条件：
+         但是这儿有个必要的前提条件：
              配置@DubboReference的类必须在需要注入对象前进行创建。
-         在spring中如何保证某个对象先创建，没有什么好方法（指定@Order是不行的），因为spring是根据相互依赖关系来创建对象的，
-         但是spring在容器创建过程中无法感知到@DubboReference，也就无法确定依赖关系。
+
+         在spring中如何保证某个对象先创建，没有什么好方法（指定@Order是不行的），
+         因为spring是根据相互依赖关系来创建对象的，但是spring在容器创建过程中无法感知到@DubboReference，也就无法确定依赖关系。
+
+         如何保证配置@DubboReference的类先创建呢？
          解决方法：
             1、配置类搜索路径，路径在前的对象会被先search出来并排在前面会被优先创建;
             eg:
